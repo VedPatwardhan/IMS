0ec94ac26e46046a14aae479dc6c2416012c45c3
Ved <vedpat3@gmail.com>
2021-11-12 13:12:30 +0530

    Fixed timeout issue of success and error messages

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -1,44 +1,55 @@
-import React, { Fragment, useState} from "react";
+import React, { Fragment } from "react";
 import { connect } from "react-redux";
 
-const ErrorMessage = function({ error }){
-  let [display, setDisplay] = useState(error);
-  console.log(display);
-  if(display && error.message){
-    let time=0;
-    const id = setInterval(()=>{
-      setDisplay(null);
-      time++;
-      if(time==2)
-        clearInterval(id);
-    }, 3000);
-  }
-  
-  return (<Fragment>
-    {display && error.message && (
-      <span
-        id="err"
-        className="alert alert-danger alert-dismissible"
-        role="alert"
-      >
-        {" "}
-        {error.message}
-        <button
-          type="button"
-          className="close"
-          data-dismiss="alert"
-          aria-label="Close"
-        >
-          <span
-            aria-hidden="true"
-            style={{ marginTop: "-10px", padding: "-20px", margin: "-10px" }}
-          >
-            &times;
-          </span>
-        </button>
-      </span>
-    )}
-  </Fragment>)
+class ErrorMessage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { display: true, id: -1};
+    }
+
+    componentDidUpdate() {
+        if (this.props.error.message) {
+            this.state.display = true;
+            clearTimeout(this.state.id);
+            this.state.id = setTimeout(() => {
+                this.setState({ display: false });
+            }, 3000);
+        }
+    }
+
+    render() {
+        return (
+            <Fragment>
+                {this.state.display && this.props.error.message && (
+                    <span
+                        id="err"
+                        className="alert alert-danger alert-dismissible"
+                        role="alert"
+                    >
+                        {" "}
+                        {this.props.error.message}
+                        <button
+                            type="button"
+                            className="close"
+                            data-dismiss="alert"
+                            aria-label="Close"
+                        >
+                            <span
+                                aria-hidden="true"
+                                style={{
+                                    marginTop: "-10px",
+                                    padding: "-20px",
+                                    margin: "-10px",
+                                }}
+                            >
+                                &times;
+                            </span>
+                        </button>
+                    </span>
+                )}
+            </Fragment>
+        );
+    }
 }
 
 export default connect((store) => ({ error: store.error }))(ErrorMessage);
--- client/src/components/SuccessMessage.jsx
+++ client/src/components/SuccessMessage.jsx
@@ -1,15 +1,32 @@
-import React, { Fragment, useState} from "react";
+import React, { Fragment, useState } from "react";
 import { connect } from "react-redux";
 
-const SuccessMessage = function( success ) {
-  return (<Fragment>
-    {success.message && (
-      <span id="err" className="alert alert-success mx-3">
-        {" "}
-        {success.message}
-      </span>
-    )}
-  </Fragment>);
+class SuccessMessage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { display: true, id: -1 };
+    }
+
+    componentDidUpdate() {
+        if (this.props.error.message) {
+            this.state.display = true;
+            clearTimeout(this.state.id);
+            this.state.id = setTimeout(() => {
+                this.setState({ display: false });
+            }, 3000);
+        }
+    }
+
+    render(){
+      return (<Fragment>
+            {this.state.display && this.props.success.message && (
+                <span id="err" className="alert alert-success mx-3">
+                    {" "}
+                    {this.props.success.message}
+                </span>
+            )}
+        </Fragment>);
+    }
 }
 
 export default connect((store) => ({ success: store.success }))(SuccessMessage);
